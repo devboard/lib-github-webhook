b38f7b7ec7bb41cde0315d90b3090ed34df6a203
Miro Svrtan <miro@mirosvrtan.me>
2018-02-12 20:17:05 +0100

    Add SampleData

---
+++ data/Core/Push/CommitCommitterSample.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core\Push;
+
+use DevboardLib\GitHubWebhook\Core\Push\CommitCommitter;
+
+class CommitCommitterSample
+{
+    private static $data = [
+        'octocat' => ['name' => 'Octo Cat', 'email' => 'octocat@example.com', 'username' => 'octocat'],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function octocat(): CommitCommitter
+    {
+        return CommitCommitter::deserialize(self::$data['octocat']);
+    }
+}
---
+++ data/Core/Push/CommitSample.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core\Push;
+
+use DevboardLib\GitHubWebhook\Core\Push\Commit;
+
+class CommitSample
+{
+    private static $data = [
+        'abc123' => [
+            'sha'           => 'abc123',
+            'message'       => 'Commit message',
+            'commitDate'    => '2018-01-01T00:01:00+00:00',
+            'author'        => ['name' => 'Octo Cat', 'email' => 'octocat@example.com', 'username' => 'octocat'],
+            'committer'     => ['name' => 'Octo Cat', 'email' => 'octocat@example.com', 'username' => 'octocat'],
+            'tree'          => 'sha',
+            'htmlUrl'       => 'htmlUrl',
+            'distinct'      => true,
+            'addedFiles'    => ['data'],
+            'modifiedFiles' => ['data'],
+            'removedFiles'  => ['data'], ],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function abc123(): Commit
+    {
+        return Commit::deserialize(self::$data['abc123']);
+    }
+}
