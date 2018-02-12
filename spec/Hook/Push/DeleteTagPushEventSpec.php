6ecfabea2ab34cf7f3bfe29f9dc07f27b61f7192
Miro Svrtan <miro@mirosvrtan.me>
2018-02-12 20:20:59 +0100

    Add PushEvents

---
+++ spec/Hook/Push/DeleteTagPushEventSpec.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\DevboardLib\GitHubWebhook\Hook\Push;
+
+use Data\DevboardLib\GitHubWebhook\Core\PusherSample;
+use Data\DevboardLib\GitHubWebhook\Core\RepoSample;
+use Data\DevboardLib\GitHubWebhook\Core\SenderSample;
+use DevboardLib\Git\Commit\CommitSha;
+use DevboardLib\GitHubWebhook\Core\Push\Ref;
+use DevboardLib\GitHubWebhook\Core\Pusher;
+use DevboardLib\GitHubWebhook\Core\Repo;
+use DevboardLib\GitHubWebhook\Core\Sender;
+use DevboardLib\GitHubWebhook\Hook\Push\DeleteTagPushEvent;
+use DevboardLib\GitHubWebhook\Hook\PushEvent;
+use PhpSpec\ObjectBehavior;
+
+class DeleteTagPushEventSpec extends ObjectBehavior
+{
+    public function let(Ref $ref, CommitSha $before, Repo $repo, Pusher $pusher, Sender $sender)
+    {
+        $this->beConstructedWith($ref, $before, $repo, $forced = false, $pusher, $sender);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(DeleteTagPushEvent::class);
+        $this->shouldImplement(PushEvent::class);
+    }
+
+    public function it_exposes_ref(Ref $ref)
+    {
+        $this->getRef()->shouldReturn($ref);
+    }
+
+    public function it_exposes_before(CommitSha $before)
+    {
+        $this->getBefore()->shouldReturn($before);
+    }
+
+    public function it_exposes_repo(Repo $repo)
+    {
+        $this->getRepo()->shouldReturn($repo);
+    }
+
+    public function it_exposes_pusher(Pusher $pusher)
+    {
+        $this->getPusher()->shouldReturn($pusher);
+    }
+
+    public function it_exposes_sender(Sender $sender)
+    {
+        $this->getSender()->shouldReturn($sender);
+    }
+
+    public function it_can_be_serialized(Ref $ref, CommitSha $before, Repo $repo, Pusher $pusher, Sender $sender)
+    {
+        $ref->serialize()->shouldBeCalled()->willReturn('refs/heads/new-feature-tag');
+        $before->serialize()->shouldBeCalled()->willReturn('sha');
+        $repo->serialize()->shouldBeCalled()->willReturn(RepoSample::serialized('octocatLinguist'));
+        $pusher->serialize()->shouldBeCalled()->willReturn(PusherSample::serialized('octocat'));
+        $sender->serialize()->shouldBeCalled()->willReturn(SenderSample::serialized('octocat'));
+        $this->serialize()->shouldReturn(
+            [
+                'ref'    => 'refs/heads/new-feature-tag',
+                'before' => 'sha',
+                'repo'   => RepoSample::serialized('octocatLinguist'),
+                'forced' => false,
+                'pusher' => PusherSample::serialized('octocat'),
+                'sender' => SenderSample::serialized('octocat'),
+            ]
+        );
+    }
+
+    public function it_can_be_deserialized()
+    {
+        $input = [
+            'ref'    => 'refs/heads/new-feature-tag',
+            'before' => 'sha',
+            'repo'   => RepoSample::serialized('octocatLinguist'),
+            'forced' => false,
+            'pusher' => PusherSample::serialized('octocat'),
+            'sender' => SenderSample::serialized('octocat'),
+        ];
+
+        $this->deserialize($input)->shouldReturnAnInstanceOf(DeleteTagPushEvent::class);
+    }
+}
---
+++ src/Hook/Push/DeleteTagPushEvent.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHubWebhook\Hook\Push;
+
+use DevboardLib\Git\Commit\CommitSha;
+use DevboardLib\GitHubWebhook\Core\Push\Ref;
+use DevboardLib\GitHubWebhook\Core\Pusher;
+use DevboardLib\GitHubWebhook\Core\Repo;
+use DevboardLib\GitHubWebhook\Core\Sender;
+use DevboardLib\GitHubWebhook\Hook\PushEvent;
+
+/**
+ * @see \spec\DevboardLib\GitHubWebhook\Hook\Push\DeleteTagPushEventSpec
+ * @see \Tests\DevboardLib\GitHubWebhook\Hook\Push\DeleteTagPushEventTest
+ */
+class DeleteTagPushEvent implements PushEvent
+{
+    /** @var Ref */
+    private $ref;
+
+    /** @var CommitSha */
+    private $before;
+
+    /** @var Repo */
+    private $repo;
+
+    /** @var bool */
+    private $forced;
+
+    /** @var Pusher */
+    private $pusher;
+
+    /** @var Sender */
+    private $sender;
+
+    public function __construct(Ref $ref, CommitSha $before, Repo $repo, bool $forced, Pusher $pusher, Sender $sender)
+    {
+        $this->ref    = $ref;
+        $this->before = $before;
+        $this->repo   = $repo;
+        $this->forced = $forced;
+        $this->pusher = $pusher;
+        $this->sender = $sender;
+    }
+
+    public function getRef(): Ref
+    {
+        return $this->ref;
+    }
+
+    public function getBefore(): CommitSha
+    {
+        return $this->before;
+    }
+
+    public function getRepo(): Repo
+    {
+        return $this->repo;
+    }
+
+    public function isForced(): bool
+    {
+        return $this->forced;
+    }
+
+    public function getPusher(): Pusher
+    {
+        return $this->pusher;
+    }
+
+    public function getSender(): Sender
+    {
+        return $this->sender;
+    }
+
+    public function serialize(): array
+    {
+        return [
+            'ref'    => $this->ref->serialize(),
+            'before' => $this->before->serialize(),
+            'repo'   => $this->repo->serialize(),
+            'forced' => $this->forced,
+            'pusher' => $this->pusher->serialize(),
+            'sender' => $this->sender->serialize(),
+        ];
+    }
+
+    public static function deserialize(array $data): self
+    {
+        return new self(
+            Ref::deserialize($data['ref']),
+            CommitSha::deserialize($data['before']),
+            Repo::deserialize($data['repo']),
+            $data['forced'],
+            Pusher::deserialize($data['pusher']),
+            Sender::deserialize($data['sender'])
+        );
+    }
+}
