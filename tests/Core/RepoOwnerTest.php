4149de299c3439e77f548e421d8618d5c8599c28
Miro Svrtan <miro@mirosvrtan.me>
2018-02-12 18:14:29 +0100

    Add RepoOwner

---
+++ tests/Core/RepoOwnerTest.php
@@ -0,0 +1,254 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DevboardLib\GitHubWebhook\Core;
+
+use DevboardLib\Generix\GravatarId;
+use DevboardLib\GitHub\Account\AccountApiUrl;
+use DevboardLib\GitHub\Account\AccountAvatarUrl;
+use DevboardLib\GitHub\Account\AccountHtmlUrl;
+use DevboardLib\GitHub\Account\AccountId;
+use DevboardLib\GitHub\Account\AccountLogin;
+use DevboardLib\GitHub\Account\AccountType;
+use DevboardLib\GitHubWebhook\Core\RepoOwner;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DevboardLib\GitHubWebhook\Core\RepoOwner
+ * @group  unit
+ */
+class RepoOwnerTest extends TestCase
+{
+    /** @var AccountId */
+    private $userId;
+
+    /** @var AccountLogin */
+    private $login;
+
+    /** @var AccountType */
+    private $type;
+
+    /** @var AccountAvatarUrl */
+    private $avatarUrl;
+
+    /** @var GravatarId */
+    private $gravatarId;
+
+    /** @var AccountHtmlUrl */
+    private $htmlUrl;
+
+    /** @var AccountApiUrl */
+    private $apiUrl;
+
+    /** @var bool */
+    private $siteAdmin;
+
+    /** @var string */
+    private $name;
+
+    /** @var string */
+    private $email;
+
+    /** @var string */
+    private $eventsUrl;
+
+    /** @var string */
+    private $followersUrl;
+
+    /** @var string */
+    private $followingUrl;
+
+    /** @var string */
+    private $gistsUrl;
+
+    /** @var string */
+    private $organizationsUrl;
+
+    /** @var string */
+    private $receivedEventsUrl;
+
+    /** @var string */
+    private $reposUrl;
+
+    /** @var string */
+    private $starredUrl;
+
+    /** @var string */
+    private $subscriptionsUrl;
+
+    /** @var RepoOwner */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->userId            = new AccountId(583231);
+        $this->login             = new AccountLogin('octocat');
+        $this->type              = new AccountType('User');
+        $this->avatarUrl         = new AccountAvatarUrl('https://avatars3.githubusercontent.com/u/583231?v=4');
+        $this->gravatarId        = new GravatarId('');
+        $this->htmlUrl           = new AccountHtmlUrl('https://github.com/octocat');
+        $this->apiUrl            = new AccountApiUrl('https://api.github.com/users/octocat');
+        $this->siteAdmin         = false;
+        $this->name              = 'octocat';
+        $this->email             = 'octocat@example.com';
+        $this->eventsUrl         = 'https://api.github.com/users/octocat/events{/privacy}';
+        $this->followersUrl      = 'https://api.github.com/users/octocat/followers';
+        $this->followingUrl      = 'https://api.github.com/users/octocat/following{/other_user}';
+        $this->gistsUrl          = 'https://api.github.com/users/octocat/gists{/gist_id}';
+        $this->organizationsUrl  = 'https://api.github.com/users/octocat/orgs';
+        $this->receivedEventsUrl = 'https://api.github.com/users/octocat/received_events';
+        $this->reposUrl          = 'https://api.github.com/users/octocat/repos';
+        $this->starredUrl        = 'https://api.github.com/users/octocat/starred{/owner}{/repo}';
+        $this->subscriptionsUrl  = 'https://api.github.com/users/octocat/subscriptions';
+        $this->sut               = new RepoOwner(
+            $this->userId,
+            $this->login,
+            $this->type,
+            $this->avatarUrl,
+            $this->gravatarId,
+            $this->htmlUrl,
+            $this->apiUrl,
+            $this->siteAdmin,
+            $this->name,
+            $this->email,
+            $this->eventsUrl,
+            $this->followersUrl,
+            $this->followingUrl,
+            $this->gistsUrl,
+            $this->organizationsUrl,
+            $this->receivedEventsUrl,
+            $this->reposUrl,
+            $this->starredUrl,
+            $this->subscriptionsUrl
+        );
+    }
+
+    public function testGetUserId()
+    {
+        self::assertSame($this->userId, $this->sut->getUserId());
+    }
+
+    public function testGetLogin()
+    {
+        self::assertSame($this->login, $this->sut->getLogin());
+    }
+
+    public function testGetType()
+    {
+        self::assertSame($this->type, $this->sut->getType());
+    }
+
+    public function testGetAvatarUrl()
+    {
+        self::assertSame($this->avatarUrl, $this->sut->getAvatarUrl());
+    }
+
+    public function testGetGravatarId()
+    {
+        self::assertSame($this->gravatarId, $this->sut->getGravatarId());
+    }
+
+    public function testGetHtmlUrl()
+    {
+        self::assertSame($this->htmlUrl, $this->sut->getHtmlUrl());
+    }
+
+    public function testGetApiUrl()
+    {
+        self::assertSame($this->apiUrl, $this->sut->getApiUrl());
+    }
+
+    public function testIsSiteAdmin()
+    {
+        self::assertSame($this->siteAdmin, $this->sut->isSiteAdmin());
+    }
+
+    public function testGetName()
+    {
+        self::assertSame($this->name, $this->sut->getName());
+    }
+
+    public function testGetEmail()
+    {
+        self::assertSame($this->email, $this->sut->getEmail());
+    }
+
+    public function testGetEventsUrl()
+    {
+        self::assertSame($this->eventsUrl, $this->sut->getEventsUrl());
+    }
+
+    public function testGetFollowersUrl()
+    {
+        self::assertSame($this->followersUrl, $this->sut->getFollowersUrl());
+    }
+
+    public function testGetFollowingUrl()
+    {
+        self::assertSame($this->followingUrl, $this->sut->getFollowingUrl());
+    }
+
+    public function testGetGistsUrl()
+    {
+        self::assertSame($this->gistsUrl, $this->sut->getGistsUrl());
+    }
+
+    public function testGetOrganizationsUrl()
+    {
+        self::assertSame($this->organizationsUrl, $this->sut->getOrganizationsUrl());
+    }
+
+    public function testGetReceivedEventsUrl()
+    {
+        self::assertSame($this->receivedEventsUrl, $this->sut->getReceivedEventsUrl());
+    }
+
+    public function testGetReposUrl()
+    {
+        self::assertSame($this->reposUrl, $this->sut->getReposUrl());
+    }
+
+    public function testGetStarredUrl()
+    {
+        self::assertSame($this->starredUrl, $this->sut->getStarredUrl());
+    }
+
+    public function testGetSubscriptionsUrl()
+    {
+        self::assertSame($this->subscriptionsUrl, $this->sut->getSubscriptionsUrl());
+    }
+
+    public function testSerialize()
+    {
+        $expected = [
+            'userId'            => 583231,
+            'login'             => 'octocat',
+            'type'              => 'User',
+            'avatarUrl'         => 'https://avatars3.githubusercontent.com/u/583231?v=4',
+            'gravatarId'        => '',
+            'htmlUrl'           => 'https://github.com/octocat',
+            'apiUrl'            => 'https://api.github.com/users/octocat',
+            'siteAdmin'         => false,
+            'name'              => 'octocat',
+            'email'             => 'octocat@example.com',
+            'eventsUrl'         => 'https://api.github.com/users/octocat/events{/privacy}',
+            'followersUrl'      => 'https://api.github.com/users/octocat/followers',
+            'followingUrl'      => 'https://api.github.com/users/octocat/following{/other_user}',
+            'gistsUrl'          => 'https://api.github.com/users/octocat/gists{/gist_id}',
+            'organizationsUrl'  => 'https://api.github.com/users/octocat/orgs',
+            'receivedEventsUrl' => 'https://api.github.com/users/octocat/received_events',
+            'reposUrl'          => 'https://api.github.com/users/octocat/repos',
+            'starredUrl'        => 'https://api.github.com/users/octocat/starred{/owner}{/repo}',
+            'subscriptionsUrl'  => 'https://api.github.com/users/octocat/subscriptions',
+        ];
+
+        self::assertSame($expected, $this->sut->serialize());
+    }
+
+    public function testDeserialize()
+    {
+        $serialized = json_encode($this->sut->serialize());
+        self::assertEquals($this->sut, RepoOwner::deserialize(json_decode($serialized, true)));
+    }
+}
