910df32e2acb6ac9331342c0f015dfa3bfadb62c
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 01:21:27 +0100

    Feat #10: Add MilestoneCreatorSample

---
+++ data/Core/Milestone/MilestoneCreatorSample.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core\Milestone;
+
+use DevboardLib\GitHub\Milestone\MilestoneCreator;
+
+class MilestoneCreatorSample
+{
+    private static $data = [
+        'octocat' => [
+            'userId'     => 1,
+            'login'      => 'octocat',
+            'type'       => 'User',
+            'avatarUrl'  => 'avatarUrl',
+            'gravatarId' => 'id',
+            'htmlUrl'    => 'htmlUrl',
+            'apiUrl'     => 'apiUrl',
+            'siteAdmin'  => true,
+        ],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function octocat(): MilestoneCreator
+    {
+        return MilestoneCreator::deserialize(self::$data['octocat']);
+    }
+}
