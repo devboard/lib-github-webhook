d3a9885db506900681796743d8d946c9f0147b0b
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 23:14:23 +0100

    Add MilestoneSample

---
+++ data/Core/MilestoneSample.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core;
+
+use DevboardLib\GitHub\GitHubMilestone;
+
+class MilestoneSample
+{
+    private static $data = [
+        'sprint1' => [
+            'id'          => 1,
+            'title'       => 'value',
+            'description' => 'value',
+            'dueOn'       => '2018-01-01T00:01:00+00:00',
+            'state'       => 'open',
+            'number'      => 1,
+            'creator'     => [
+                'userId'     => 1,
+                'login'      => 'octocat',
+                'type'       => 'User',
+                'avatarUrl'  => 'avatarUrl',
+                'gravatarId' => 'id',
+                'htmlUrl'    => 'htmlUrl',
+                'apiUrl'     => 'apiUrl',
+                'siteAdmin'  => true,
+            ],
+            'htmlUrl'   => 'htmlUrl',
+            'apiUrl'    => 'apiUrl',
+            'closedAt'  => '2018-01-01T00:01:00+00:00',
+            'createdAt' => '2018-01-01T00:01:00+00:00',
+            'updatedAt' => '2018-01-01T00:01:00+00:00',
+        ],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function sprint1(): GitHubMilestone
+    {
+        return GitHubMilestone::deserialize(self::$data['sprint1']);
+    }
+}
