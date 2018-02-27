aa71ee25c76f202bcfd4a858c0ae7158a98cbf99
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 10:54:01 +0100

    Feat #9: Add Label events

---
+++ spec/Hook/Label/CreatedLabelEventSpec.php
@@ -0,0 +1,339 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\DevboardLib\GitHubWebhook\Hook\Label;
+
+use Data\DevboardLib\GitHubWebhook\Core\RepoSample;
+use DevboardLib\GitHub\GitHubLabel;
+use DevboardLib\GitHub\Installation\InstallationId;
+use DevboardLib\GitHubWebhook\Core\Repo;
+use DevboardLib\GitHubWebhook\Core\Sender;
+use DevboardLib\GitHubWebhook\Hook\Label\CreatedLabelEvent;
+use DevboardLib\GitHubWebhook\Hook\Label\LabelEvent;
+use PhpSpec\ObjectBehavior;
+
+class CreatedLabelEventSpec extends ObjectBehavior
+{
+    public function let(GitHubLabel $label, Repo $repo, InstallationId $installationId, Sender $sender)
+    {
+        $this->beConstructedWith($label, $repo, $installationId, $sender);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(CreatedLabelEvent::class);
+        $this->shouldImplement(LabelEvent::class);
+    }
+
+    public function it_exposes_label(GitHubLabel $label)
+    {
+        $this->getLabel()->shouldReturn($label);
+    }
+
+    public function it_exposes_repo(Repo $repo)
+    {
+        $this->getRepo()->shouldReturn($repo);
+    }
+
+    public function it_exposes_installation_id(InstallationId $installationId)
+    {
+        $this->getInstallationId()->shouldReturn($installationId);
+    }
+
+    public function it_exposes_sender(Sender $sender)
+    {
+        $this->getSender()->shouldReturn($sender);
+    }
+
+    public function it_can_be_serialized(
+        GitHubLabel $label, Repo $repo, InstallationId $installationId, Sender $sender
+    ) {
+        $label->serialize()->shouldBeCalled()->willReturn(
+            ['id' => 1, 'name' => 'value', 'color' => 'color', 'default' => true, 'apiUrl' => 'apiUrl']
+        );
+        $repo->serialize()->shouldBeCalled()->willReturn(
+            [
+                'id'       => 1,
+                'fullName' => ['owner' => 'value', 'repoName' => 'name'],
+                'owner'    => [
+                    'userId'            => 1,
+                    'login'             => 'value',
+                    'type'              => 'User',
+                    'avatarUrl'         => 'avatarUrl',
+                    'gravatarId'        => 'id',
+                    'htmlUrl'           => 'htmlUrl',
+                    'apiUrl'            => 'apiUrl',
+                    'siteAdmin'         => true,
+                    'name'              => 'name',
+                    'email'             => 'email',
+                    'eventsUrl'         => 'eventsUrl',
+                    'followersUrl'      => 'followersUrl',
+                    'followingUrl'      => 'followingUrl',
+                    'gistsUrl'          => 'gistsUrl',
+                    'organizationsUrl'  => 'organizationsUrl',
+                    'receivedEventsUrl' => 'receivedEventsUrl',
+                    'reposUrl'          => 'reposUrl',
+                    'starredUrl'        => 'starredUrl',
+                    'subscriptionsUrl'  => 'subscriptionsUrl',
+                ],
+                'private'       => true,
+                'defaultBranch' => 'name',
+                'fork'          => true,
+                'parent'        => ['id' => 1, 'fullName' => ['owner' => 'value', 'repoName' => 'name']],
+                'description'   => 'description',
+                'homepage'      => 'homepage',
+                'language'      => 'language',
+                'mirrorUrl'     => 'mirrorUrl',
+                'archived'      => true,
+                'endpoints'     => [
+                    'htmlUrl' => 'htmlUrl',
+                    'apiUrl'  => 'apiUrl',
+                    'gitUrl'  => 'gitUrl',
+                    'sshUrl'  => 'sshUrl',
+                ],
+                'stats' => [
+                    'networkCount'     => 1,
+                    'watchersCount'    => 1,
+                    'stargazersCount'  => 1,
+                    'subscribersCount' => 1,
+                    'openIssuesCount'  => 1,
+                    'size'             => 1,
+                ],
+                'timestamps' => [
+                    'createdAt' => '2018-01-01T00:01:00+00:00',
+                    'updatedAt' => '2018-01-01T00:01:00+00:00',
+                    'pushedAt'  => '2018-01-01T00:01:00+00:00',
+                ],
+                'additionalDetails' => [
+                    'license'      => 'master',
+                    'forksCount'   => 0,
+                    'hasDownloads' => true,
+                    'hasIssues'    => true,
+                    'hasPages'     => true,
+                    'hasProjects'  => true,
+                    'hasWiki'      => true,
+                ],
+                'repoAdditionalUrls' => [
+                    'archiveUrl'       => 'https://api.github.com/repos/octocat/linguist/{archive_format}{/ref}',
+                    'assigneesUrl'     => 'https://api.github.com/repos/octocat/linguist/assignees{/user}',
+                    'blobsUrl'         => 'https://api.github.com/repos/octocat/linguist/git/blobs{/sha}',
+                    'branchesUrl'      => 'https://api.github.com/repos/octocat/linguist/branches{/branch}',
+                    'cloneUrl'         => 'https://github.com/octocat/linguist.git',
+                    'collaboratorsUrl' => 'https://api.github.com/repos/octocat/linguist/collaborators{/collaborator}',
+                    'commentsUrl'      => 'https://api.github.com/repos/octocat/linguist/comments{/number}',
+                    'commitsUrl'       => 'https://api.github.com/repos/octocat/linguist/commits{/sha}',
+                    'compareUrl'       => 'https://api.github.com/repos/octocat/linguist/compare/{base}...{head}',
+                    'contentsUrl'      => 'https://api.github.com/repos/octocat/linguist/contents/{+path}',
+                    'contributorsUrl'  => 'https://api.github.com/repos/octocat/linguist/contributors',
+                    'deploymentsUrl'   => 'https://api.github.com/repos/octocat/linguist/deployments',
+                    'downloadsUrl'     => 'https://api.github.com/repos/octocat/linguist/downloads',
+                    'eventsUrl'        => 'https://api.github.com/repos/octocat/linguist/events',
+                    'forksUrl'         => 'https://api.github.com/repos/octocat/linguist/forks',
+                    'gitCommitsUrl'    => 'https://api.github.com/repos/octocat/linguist/git/commits{/sha}',
+                    'gitRefsUrl'       => 'https://api.github.com/repos/octocat/linguist/git/refs{/sha}',
+                    'gitTagsUrl'       => 'https://api.github.com/repos/octocat/linguist/git/tags{/sha}',
+                    'hooksUrl'         => 'https://api.github.com/repos/octocat/linguist/hooks',
+                    'issueCommentUrl'  => 'https://api.github.com/repos/octocat/linguist/issues/comments{/number}',
+                    'issueEventsUrl'   => 'https://api.github.com/repos/octocat/linguist/issues/events{/number}',
+                    'issuesUrl'        => 'https://api.github.com/repos/octocat/linguist/issues{/number}',
+                    'keysUrl'          => 'https://api.github.com/repos/octocat/linguist/keys{/key_id}',
+                    'labelsUrl'        => 'https://api.github.com/repos/octocat/linguist/labels{/name}',
+                    'languagesUrl'     => 'https://api.github.com/repos/octocat/linguist/languages',
+                    'mergesUrl'        => 'https://api.github.com/repos/octocat/linguist/merges',
+                    'milestonesUrl'    => 'https://api.github.com/repos/octocat/linguist/milestones{/number}',
+                    'notificationsUrl' => 'https://api.github.com/repos/octocat/linguist/notifications{?since,all,participating}',
+                    'pullsUrl'         => 'https://api.github.com/repos/octocat/linguist/pulls{/number}',
+                    'releasesUrl'      => 'https://api.github.com/repos/octocat/linguist/releases{/id}',
+                    'stargazersUrl'    => 'https://api.github.com/repos/octocat/linguist/stargazers',
+                    'statusesUrl'      => 'https://api.github.com/repos/octocat/linguist/statuses/{sha}',
+                    'subscribersUrl'   => 'https://api.github.com/repos/octocat/linguist/subscribers',
+                    'subscriptionUrl'  => 'https://api.github.com/repos/octocat/linguist/subscription',
+                    'tagsUrl'          => 'https://api.github.com/repos/octocat/linguist/tags',
+                    'teamsUrl'         => 'https://api.github.com/repos/octocat/linguist/teams',
+                    'treesUrl'         => 'https://api.github.com/repos/octocat/linguist/git/trees{/sha}',
+                    'svnUrl'           => 'https://github.com/octocat/linguist',
+                ],
+            ]
+        );
+        $installationId->serialize()->shouldBeCalled()->willReturn(1);
+        $sender->serialize()->shouldBeCalled()->willReturn(
+            [
+                'userId'            => 1,
+                'login'             => 'value',
+                'type'              => 'User',
+                'avatarUrl'         => 'avatarUrl',
+                'gravatarId'        => 'id',
+                'htmlUrl'           => 'htmlUrl',
+                'apiUrl'            => 'apiUrl',
+                'siteAdmin'         => true,
+                'eventsUrl'         => 'eventsUrl',
+                'followersUrl'      => 'followersUrl',
+                'followingUrl'      => 'followingUrl',
+                'gistsUrl'          => 'gistsUrl',
+                'organizationsUrl'  => 'organizationsUrl',
+                'receivedEventsUrl' => 'receivedEventsUrl',
+                'reposUrl'          => 'reposUrl',
+                'starredUrl'        => 'starredUrl',
+                'subscriptionsUrl'  => 'subscriptionsUrl',
+            ]
+        );
+        $this->serialize()->shouldReturn(
+            [
+                'label' => ['id' => 1, 'name' => 'value', 'color' => 'color', 'default' => true, 'apiUrl' => 'apiUrl'],
+                'repo'  => [
+                    'id'       => 1,
+                    'fullName' => ['owner' => 'value', 'repoName' => 'name'],
+                    'owner'    => [
+                        'userId'            => 1,
+                        'login'             => 'value',
+                        'type'              => 'User',
+                        'avatarUrl'         => 'avatarUrl',
+                        'gravatarId'        => 'id',
+                        'htmlUrl'           => 'htmlUrl',
+                        'apiUrl'            => 'apiUrl',
+                        'siteAdmin'         => true,
+                        'name'              => 'name',
+                        'email'             => 'email',
+                        'eventsUrl'         => 'eventsUrl',
+                        'followersUrl'      => 'followersUrl',
+                        'followingUrl'      => 'followingUrl',
+                        'gistsUrl'          => 'gistsUrl',
+                        'organizationsUrl'  => 'organizationsUrl',
+                        'receivedEventsUrl' => 'receivedEventsUrl',
+                        'reposUrl'          => 'reposUrl',
+                        'starredUrl'        => 'starredUrl',
+                        'subscriptionsUrl'  => 'subscriptionsUrl',
+                    ],
+                    'private'       => true,
+                    'defaultBranch' => 'name',
+                    'fork'          => true,
+                    'parent'        => ['id' => 1, 'fullName' => ['owner' => 'value', 'repoName' => 'name']],
+                    'description'   => 'description',
+                    'homepage'      => 'homepage',
+                    'language'      => 'language',
+                    'mirrorUrl'     => 'mirrorUrl',
+                    'archived'      => true,
+                    'endpoints'     => [
+                        'htmlUrl' => 'htmlUrl',
+                        'apiUrl'  => 'apiUrl',
+                        'gitUrl'  => 'gitUrl',
+                        'sshUrl'  => 'sshUrl',
+                    ],
+                    'stats' => [
+                        'networkCount'     => 1,
+                        'watchersCount'    => 1,
+                        'stargazersCount'  => 1,
+                        'subscribersCount' => 1,
+                        'openIssuesCount'  => 1,
+                        'size'             => 1,
+                    ],
+                    'timestamps' => [
+                        'createdAt' => '2018-01-01T00:01:00+00:00',
+                        'updatedAt' => '2018-01-01T00:01:00+00:00',
+                        'pushedAt'  => '2018-01-01T00:01:00+00:00',
+                    ],
+                    'additionalDetails' => [
+                        'license'      => 'master',
+                        'forksCount'   => 0,
+                        'hasDownloads' => true,
+                        'hasIssues'    => true,
+                        'hasPages'     => true,
+                        'hasProjects'  => true,
+                        'hasWiki'      => true,
+                    ],
+                    'repoAdditionalUrls' => [
+                        'archiveUrl'       => 'https://api.github.com/repos/octocat/linguist/{archive_format}{/ref}',
+                        'assigneesUrl'     => 'https://api.github.com/repos/octocat/linguist/assignees{/user}',
+                        'blobsUrl'         => 'https://api.github.com/repos/octocat/linguist/git/blobs{/sha}',
+                        'branchesUrl'      => 'https://api.github.com/repos/octocat/linguist/branches{/branch}',
+                        'cloneUrl'         => 'https://github.com/octocat/linguist.git',
+                        'collaboratorsUrl' => 'https://api.github.com/repos/octocat/linguist/collaborators{/collaborator}',
+                        'commentsUrl'      => 'https://api.github.com/repos/octocat/linguist/comments{/number}',
+                        'commitsUrl'       => 'https://api.github.com/repos/octocat/linguist/commits{/sha}',
+                        'compareUrl'       => 'https://api.github.com/repos/octocat/linguist/compare/{base}...{head}',
+                        'contentsUrl'      => 'https://api.github.com/repos/octocat/linguist/contents/{+path}',
+                        'contributorsUrl'  => 'https://api.github.com/repos/octocat/linguist/contributors',
+                        'deploymentsUrl'   => 'https://api.github.com/repos/octocat/linguist/deployments',
+                        'downloadsUrl'     => 'https://api.github.com/repos/octocat/linguist/downloads',
+                        'eventsUrl'        => 'https://api.github.com/repos/octocat/linguist/events',
+                        'forksUrl'         => 'https://api.github.com/repos/octocat/linguist/forks',
+                        'gitCommitsUrl'    => 'https://api.github.com/repos/octocat/linguist/git/commits{/sha}',
+                        'gitRefsUrl'       => 'https://api.github.com/repos/octocat/linguist/git/refs{/sha}',
+                        'gitTagsUrl'       => 'https://api.github.com/repos/octocat/linguist/git/tags{/sha}',
+                        'hooksUrl'         => 'https://api.github.com/repos/octocat/linguist/hooks',
+                        'issueCommentUrl'  => 'https://api.github.com/repos/octocat/linguist/issues/comments{/number}',
+                        'issueEventsUrl'   => 'https://api.github.com/repos/octocat/linguist/issues/events{/number}',
+                        'issuesUrl'        => 'https://api.github.com/repos/octocat/linguist/issues{/number}',
+                        'keysUrl'          => 'https://api.github.com/repos/octocat/linguist/keys{/key_id}',
+                        'labelsUrl'        => 'https://api.github.com/repos/octocat/linguist/labels{/name}',
+                        'languagesUrl'     => 'https://api.github.com/repos/octocat/linguist/languages',
+                        'mergesUrl'        => 'https://api.github.com/repos/octocat/linguist/merges',
+                        'milestonesUrl'    => 'https://api.github.com/repos/octocat/linguist/milestones{/number}',
+                        'notificationsUrl' => 'https://api.github.com/repos/octocat/linguist/notifications{?since,all,participating}',
+                        'pullsUrl'         => 'https://api.github.com/repos/octocat/linguist/pulls{/number}',
+                        'releasesUrl'      => 'https://api.github.com/repos/octocat/linguist/releases{/id}',
+                        'stargazersUrl'    => 'https://api.github.com/repos/octocat/linguist/stargazers',
+                        'statusesUrl'      => 'https://api.github.com/repos/octocat/linguist/statuses/{sha}',
+                        'subscribersUrl'   => 'https://api.github.com/repos/octocat/linguist/subscribers',
+                        'subscriptionUrl'  => 'https://api.github.com/repos/octocat/linguist/subscription',
+                        'tagsUrl'          => 'https://api.github.com/repos/octocat/linguist/tags',
+                        'teamsUrl'         => 'https://api.github.com/repos/octocat/linguist/teams',
+                        'treesUrl'         => 'https://api.github.com/repos/octocat/linguist/git/trees{/sha}',
+                        'svnUrl'           => 'https://github.com/octocat/linguist',
+                    ],
+                ],
+                'installationId' => 1,
+                'sender'         => [
+                    'userId'            => 1,
+                    'login'             => 'value',
+                    'type'              => 'User',
+                    'avatarUrl'         => 'avatarUrl',
+                    'gravatarId'        => 'id',
+                    'htmlUrl'           => 'htmlUrl',
+                    'apiUrl'            => 'apiUrl',
+                    'siteAdmin'         => true,
+                    'eventsUrl'         => 'eventsUrl',
+                    'followersUrl'      => 'followersUrl',
+                    'followingUrl'      => 'followingUrl',
+                    'gistsUrl'          => 'gistsUrl',
+                    'organizationsUrl'  => 'organizationsUrl',
+                    'receivedEventsUrl' => 'receivedEventsUrl',
+                    'reposUrl'          => 'reposUrl',
+                    'starredUrl'        => 'starredUrl',
+                    'subscriptionsUrl'  => 'subscriptionsUrl',
+                ],
+            ]
+        );
+    }
+
+    public function it_can_be_deserialized()
+    {
+        $input = [
+            'label'          => ['id' => 1, 'name' => 'value', 'color' => 'color', 'default' => true, 'apiUrl' => 'apiUrl'],
+            'repo'           => RepoSample::serialized('octocatLinguist'),
+            'installationId' => 1,
+            'sender'         => [
+                'userId'            => 1,
+                'login'             => 'value',
+                'type'              => 'User',
+                'avatarUrl'         => 'avatarUrl',
+                'gravatarId'        => 'id',
+                'htmlUrl'           => 'htmlUrl',
+                'apiUrl'            => 'apiUrl',
+                'siteAdmin'         => true,
+                'eventsUrl'         => 'eventsUrl',
+                'followersUrl'      => 'followersUrl',
+                'followingUrl'      => 'followingUrl',
+                'gistsUrl'          => 'gistsUrl',
+                'organizationsUrl'  => 'organizationsUrl',
+                'receivedEventsUrl' => 'receivedEventsUrl',
+                'reposUrl'          => 'reposUrl',
+                'starredUrl'        => 'starredUrl',
+                'subscriptionsUrl'  => 'subscriptionsUrl',
+            ],
+        ];
+
+        $this->deserialize($input)->shouldReturnAnInstanceOf(CreatedLabelEvent::class);
+    }
+}
---
+++ src/Hook/Label/DeletedLabelEvent.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHubWebhook\Hook\Label;
+
+use DevboardLib\GitHub\GitHubLabel;
+use DevboardLib\GitHub\Installation\InstallationId;
+use DevboardLib\GitHubWebhook\Core\Repo;
+use DevboardLib\GitHubWebhook\Core\Sender;
+
+/**
+ * @see \spec\DevboardLib\GitHubWebhook\Hook\Label\DeletedLabelEventSpec
+ * @see \Tests\DevboardLib\GitHubWebhook\Hook\Label\DeletedLabelEventTest
+ */
+class DeletedLabelEvent implements LabelEvent
+{
+    /** @var GitHubLabel */
+    private $label;
+
+    /** @var Repo */
+    private $repo;
+
+    /** @var InstallationId */
+    private $installationId;
+
+    /** @var Sender */
+    private $sender;
+
+    public function __construct(GitHubLabel $label, Repo $repo, InstallationId $installationId, Sender $sender)
+    {
+        $this->label          = $label;
+        $this->repo           = $repo;
+        $this->installationId = $installationId;
+        $this->sender         = $sender;
+    }
+
+    public function getLabel(): GitHubLabel
+    {
+        return $this->label;
+    }
+
+    public function getRepo(): Repo
+    {
+        return $this->repo;
+    }
+
+    public function getInstallationId(): InstallationId
+    {
+        return $this->installationId;
+    }
+
+    public function getSender(): Sender
+    {
+        return $this->sender;
+    }
+
+    public function serialize(): array
+    {
+        return [
+            'label'          => $this->label->serialize(),
+            'repo'           => $this->repo->serialize(),
+            'installationId' => $this->installationId->serialize(),
+            'sender'         => $this->sender->serialize(),
+        ];
+    }
+
+    public static function deserialize(array $data): self
+    {
+        return new self(
+            GitHubLabel::deserialize($data['label']),
+            Repo::deserialize($data['repo']),
+            InstallationId::deserialize($data['installationId']),
+            Sender::deserialize($data['sender'])
+        );
+    }
+}
