3a51e4faf1ce1dd0fb486c1fa84d2b87f47854fa
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 10:40:50 +0100

    Feat #8: Add IssueComment events

---
+++ data/Core/IssueComment/IssueCommentAuthorSample.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core\IssueComment;
+
+use DevboardLib\GitHub\IssueComment\IssueCommentAuthor;
+
+class IssueCommentAuthorSample
+{
+    private static $data = [
+        'octocat' => [
+            'userId'     => 1,
+            'login'      => 'octocat',
+            'type'       => 'User',
+            'avatarUrl'  => 'avatarUrl',
+            'gravatarId' => 'id',
+            'htmlUrl'    => 'htmlUrl',
+            'apiUrl'     => 'apiUrl',
+            'siteAdmin'  => true,
+        ],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function octocat(): IssueCommentAuthor
+    {
+        return IssueCommentAuthor::deserialize(self::$data['octocat']);
+    }
+}
---
+++ src/Hook/IssueComment/IssueCommentEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHubWebhook\Hook\IssueComment;
+
+use DevboardLib\GitHubWebhook\Hook\PushEvent;
+
+interface IssueCommentEvent extends PushEvent
+{
+}
