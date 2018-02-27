116c8691fb90fa11dd89588ca7bcf11e95472ac0
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 10:40:38 +0100

    Feat #7: Add Issue events

---
+++ src/Hook/Issue/IssueEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHubWebhook\Hook\Issue;
+
+use DevboardLib\GitHubWebhook\Hook\PushEvent;
+
+interface IssueEvent extends PushEvent
+{
+}
