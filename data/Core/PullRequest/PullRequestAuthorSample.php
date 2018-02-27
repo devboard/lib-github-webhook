b8b92f8ea89685899b74f66605e86fc9d0a3cfd3
Miro Svrtan <miro@mirosvrtan.me>
2018-02-27 01:21:47 +0100

    Feat #10: Add PullRequestAuthorSample

---
+++ data/Core/PullRequest/PullRequestAuthorSample.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data\DevboardLib\GitHubWebhook\Core\PullRequest;
+
+use DevboardLib\GitHub\PullRequest\PullRequestAuthor;
+
+class PullRequestAuthorSample
+{
+    private static $data = [
+        'octocat' => [
+            'userId'     => 1,
+            'login'      => 'octocat',
+            'type'       => 'User',
+            'avatarUrl'  => 'avatarUrl',
+            'gravatarId' => 'id',
+            'htmlUrl'    => 'htmlUrl',
+            'apiUrl'     => 'apiUrl',
+            'siteAdmin'  => true,
+        ],
+    ];
+
+    public static function serialized(string $item): array
+    {
+        return self::$data[$item];
+    }
+
+    public static function octocat(): PullRequestAuthor
+    {
+        return PullRequestAuthor::deserialize(self::$data['octocat']);
+    }
+}
