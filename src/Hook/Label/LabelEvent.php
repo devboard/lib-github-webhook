a29e084bbce96e7642f277889b1aeff28c51a27e
Miro Svrtan <miro@mirosvrtan.me>
2018-03-03 19:50:54 +0100

    Add missing GitHubHookEvent interfaces

--- src/Hook/Label/LabelEvent.php
+++ src/Hook/Label/LabelEvent.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook\Label;
 
-interface LabelEvent
+use DevboardLib\GitHubWebhook\Hook\GitHubHookEvent;
+
+interface LabelEvent extends GitHubHookEvent
 {
 }
--- src/Hook/PullRequest/PullRequestEvent.php
+++ src/Hook/PullRequest/PullRequestEvent.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook\PullRequest;
 
-interface PullRequestEvent
+use DevboardLib\GitHubWebhook\Hook\GitHubHookEvent;
+
+interface PullRequestEvent extends GitHubHookEvent
 {
 }
