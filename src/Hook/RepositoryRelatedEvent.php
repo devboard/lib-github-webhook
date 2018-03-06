0f78a34a0335c41e4388ca75f51121f3e1bf58f2
Miro Svrtan <miro@mirosvrtan.me>
2018-03-06 23:17:18 +0100

    RepositoryRelatedEvent's should expose RepoId & RepoFullName #60

--- src/Hook/RepositoryRelatedEvent.php
+++ src/Hook/RepositoryRelatedEvent.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook;
 
+use DevboardLib\GitHub\Repo\RepoFullName;
+use DevboardLib\GitHub\Repo\RepoId;
 use DevboardLib\GitHubWebhook\Core\Repo;
 
 interface RepositoryRelatedEvent extends GitHubHookEvent
 {
     public function getRepo(): Repo;
+
+    public function getRepoId(): RepoId;
+
+    public function getRepoFullName(): RepoFullName;
 }
