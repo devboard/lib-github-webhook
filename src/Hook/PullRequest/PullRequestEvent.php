d50eb5befe42b023e29259c6a25015bc35cff282
Miro Svrtan <miro@mirosvrtan.me>
2018-03-27 07:34:44 +0200

    Task #76: PullRequestEvent should have getPullRequest

--- src/Hook/PullRequest/PullRequestEvent.php
+++ src/Hook/PullRequest/PullRequestEvent.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook\PullRequest;
 
+use DevboardLib\GitHubWebhook\Core\PullRequest\PullRequest;
 use DevboardLib\GitHubWebhook\Hook\RepositoryRelatedEvent;
 
 interface PullRequestEvent extends RepositoryRelatedEvent
 {
+    public function getPullRequest(): PullRequest;
 }
