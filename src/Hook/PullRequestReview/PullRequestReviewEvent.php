e19f2c98a2efb4ac4077561f6ba2a00b3b26e7bd
Miro Svrtan <miro@mirosvrtan.me>
2018-03-28 17:57:02 +0200

    Add method expectations to PullRequestReviewEvent

--- src/Hook/PullRequestReview/PullRequestReviewEvent.php
+++ src/Hook/PullRequestReview/PullRequestReviewEvent.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook\PullRequestReview;
 
+use DevboardLib\GitHubWebhook\Core\PullRequest\PullRequest;
+use DevboardLib\GitHubWebhook\Core\PullRequestReview\PullRequestReview;
 use DevboardLib\GitHubWebhook\Hook\RepositoryRelatedEvent;
 
 interface PullRequestReviewEvent extends RepositoryRelatedEvent
 {
+    public function getReview(): PullRequestReview;
+
+    public function getPullRequest(): PullRequest;
 }
