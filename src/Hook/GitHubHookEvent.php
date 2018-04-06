5f94464540eb52760556f4e64a5898a363392dd9
Miro Svrtan <miro@mirosvrtan.me>
2018-04-06 21:39:04 +0200

    Add GitHubHookEvent->getSender() expectation

--- src/Hook/GitHubHookEvent.php
+++ src/Hook/GitHubHookEvent.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubWebhook\Hook;
 
+use DevboardLib\GitHubWebhook\Core\Sender;
+
 interface GitHubHookEvent
 {
+    public function getSender(): Sender;
 }
