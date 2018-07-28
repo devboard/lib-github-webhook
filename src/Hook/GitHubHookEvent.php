6a0b384567c94f786860aa999c9774b2d6056235
Miro Svrtan <miro@mirosvrtan.me>
2018-07-28 21:52:13 +0200

    GitHubHookEvent should define serialize/deserialize methods

--- src/Hook/GitHubHookEvent.php
+++ src/Hook/GitHubHookEvent.php
@@ -9,4 +9,9 @@ use DevboardLib\GitHubWebhook\Core\Sender;
 interface GitHubHookEvent
 {
     public function getSender(): Sender;
+
+    public function serialize(): array;
+
+    /** @return static */
+    public static function deserialize(array $data);
 }
